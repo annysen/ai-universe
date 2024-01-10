7c8067353b0ab3af58de79d0d3d515cab4fe6936
annysen <annysen95@gmail.com>
2024-01-10 21:48:19 +0600

    card section added

--- src/component/SingleCard.jsx
+++ src/component/SingleCard.jsx
@@ -6,16 +6,13 @@ const SingleCard = (props) => {
   const { name, description, image, features } = props.singleData;
   return (
     <>
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+      <div className="card card-compact w-96 bg-base-100 shadow-2xl">
         <figure>
-          <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
+          <img className="w-full" src={image} />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <h2 className="card-title">{name}</h2>
+          <p>{description}</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
           </div>
